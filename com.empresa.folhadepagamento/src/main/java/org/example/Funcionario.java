aa016c6445332aff3dcf2418bd7b700ce267e065
Letícia Paula de Oliveira <167941977+leti-poliveira@users.noreply.github.com>
2024-06-27 19:04:19 -0300

    Funcionario.java

--- com.empresa.folhadepagamento/src/main/java/org/example/Funcionario.java
+++ com.empresa.folhadepagamento/src/main/java/org/example/Funcionario.java
@@ -3,10 +3,12 @@ package org.example;
 public class Funcionario {
     private String nome;
     private double salarioBruto;
+    private double valeTransporte;
 
     public Funcionario(String nome, double salarioBruto) {
         this.nome = nome;
         this.salarioBruto = salarioBruto;
+        this.valeTransporte = 0.0; // Inicializa o VT como zero
     }
 
     // Getters e Setters
@@ -24,5 +26,13 @@ public class Funcionario {
 
     public void setSalarioBruto(double salarioBruto) {
         this.salarioBruto = salarioBruto;
+    }
+
+    public double getValeTransporte() {
+        return valeTransporte;
+    }
+
+    public void setValeTransporte(double valeTransporte) {
+        this.valeTransporte = valeTransporte;
     }
 }
